0feb9cfe76c5334d1b4002dc2ee05150403b04aa
Ted Tschopp <ted@tschopp.org>
2017-05-28 19:53:00 -0700

    biblical_curses.js

---
+++ tables/biblical_curses.js
@@ -0,0 +1,84 @@
+gen_data['biblical_curses'] = [
+  '{bibilcal_curses_mayYou} {bibilcal_curses_haveBadThingsHappen}, {bibilcal_curses_ohYou} {bibilcal_curses_ofLittleFaith}!',
+  '{bibilcal_curses_hearThis}, {bibilcal_curses_ohYou} {bibilcal_curses_ofLittleFaith}, for you will {bibilcal_curses_haveBadThingsHappen}!'
+]
+
+gen_data['bibilcal_curses_mayYou'] = [
+  'I pray thou shalt',
+  'I hope you will',
+  'Behold, thou shalt',
+  'May you',
+  'Thou shalt',
+  'O that thou wouldest'
+]
+gen_data['bibilcal_curses_haveBadThingsHappen'] = [
+  "have more mother-in-laws than King Solomon",
+  "be pursued into the mountains by sex-mad baboons",
+  "be whipped with a thousand scorpions",
+  "be thrown into a den of hyperactive lions",
+  "be swallowed by a whale with excessively bad breath",
+  "be smitten with all-over boils",
+  "be mocked by eunuchs",
+  "be captured by Midianite maniacs",
+  "become as popular as a boil on the king's backside",
+  "be plagued with gnats, flies and locusts",
+  "be taunted by the king's concubines",
+  "fall under a speeding chariot",
+  "be as welcome as a fart in the queen's bedchamber",
+  "accidentally insult Goliath",
+  "go about weeping and wailing in sackcloth and ashes",
+  "go on a diet of crunchy, unsweetened locusts",
+  "be trampled by a herd of stampeding pigs",
+  "be cast onto a steaming dung-heap",
+  "be turned into a pillar of salt",
+  "see your pomegranates wither",
+  "beget difficult teenagers",
+  "be kicked by an incontinent camel",
+  "crash the king's best-loved chariot"
+]
+gen_data['bibilcal_curses_ohYou'] = [
+  "thou",
+  "O thou",
+  "O ye"
+]
+
+gen_data['bibilcal_curses_ofLittleFaith'] = [
+  "of little faith",
+  "whose name is but dung",
+  "who art a byword for idiocy",
+  "breaker of the commandments",
+  "discourager of the brethren",
+  "wolf in sheep's clothing",
+  "sad Pharisee",
+  "armpit of Satan",
+  "irritating inhabitant of Gath",
+  "child of Jezebel",
+  "son of thunder",
+  "relative of Herod",
+  "incompetent tax-collector",
+  "lazy Babylonian",
+  "babbling Assyrian",
+  "Amalekite dog",
+  "lying Girgashite",
+  "love-crazed Gittite",
+  "creature of the pit",
+  "bull of Bashan",
+  "Mesopotomian harlot",
+  "wayward winebibber",
+  "son of a Philistine",
+  "sulphurous nonentity",
+  "love-child of Methuselah",
+  "plaything of Beelzebub",
+  "dabbler in abominations",
+  "exceedingly foolish virgin",
+  "denizen of the underworld",
+  "offspring of a squashed cockroach"
+]
+
+gen_data['bibilcal_curses_hearThis'] = [
+  "Listen",
+  "Hear this",
+  "Take heed",
+  "Woe unto thee",
+  "Harken"
+]
