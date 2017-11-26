b0c695b9f1979a95238b9339466d59e411c206fb
Ted Tschopp <ted@tschopp.org>
2017-11-26 14:02:48 -0800

    UTF-8

--- tables/names_Polish.js
+++ tables/names_Polish.js
@@ -1,8 +1,8 @@
 gen_data["Polish_male_common"] = [
-'Aleksander', 'Fryderyk', 'Henryk', 'Jakub', 'Klement', 'Konrad', 'Krzysztof', 'Lorenz', 'M·ty·s', 'Tymoteusz', 'Wilhelm', 'Wincenty'
+'Aleksander', 'Fryderyk', 'Henryk', 'Jakub', 'Klement', 'Konrad', 'Krzysztof', 'Lorenz', 'M√°ty√°s', 'Tymoteusz', 'Wilhelm', 'Wincenty'
 ]
 gen_data["Polish_female_common"] = [
-'Agnieszk·', 'Ann·', 'Barbar·', 'Dorot·', 'Giertrud·', 'Jadwig·', 'Katarzyn·', 'Regin·', 'Rozalj·', 'Stefani·', 'Weronik·', 'Zofja'
+'Agnieszk√°', 'Ann√°', 'Barbar√°', 'Dorot√°', 'Giertrud√°', 'Jadwig√°', 'Katarzyn√°', 'Regin√°', 'Rozalj√°', 'Stefani√°', 'Weronik√°', 'Zofja'
 ]
 
 
@@ -12,6 +12,3 @@ gen_data["Polish_male_name"] = {
 gen_data["Polish_female_name"] = {
     '1-80': '{Polish_female_common}'
 }
-
-
-
