60dc6f3c620b58d064746e8f233a9c131a745e44
Ted Tschopp <ted@tschopp.org>
2017-11-26 13:59:28 -0800

    UTF-8

--- tables/names_French.js
+++ tables/names_French.js
@@ -2,13 +2,13 @@ gen_data["French_General_male_common"] = [
 'Baudouin', 'Gautier', 'Geoffroy', 'Guillaume', 'Guy', 'Henri', 'Hugues', 'Jean', 'Pierre', 'Robert'
 ]
 gen_data["French_General_male_uncommon"] = [
-'Abélard', 'Adhémar', 'Alain', 'Albert', 'Alizandre', 'Amélien', 'Amiel', 'André', 'Anselm', 'Archambaud', 'Arnaud', 'Aubert', 'Aubry', 'Baldwin', 'Barthélemy', 'Benet', 'Benoît', 'Bérenger', 'Bernard', 'Berthomieu', 'Bertrand', 'Chrétien', 'Constantin', 'Emmanuel', 'Erard', 'Eustace', 'Evre', 'Frédéric', 'Fulbert', 'Fulk', 'Gaillard', 'Galland', 'Gaston', 'Gaudri', 'Gautier', 'Geoffroy', 'Gerbert', 'Germain', 'Girard', 'Girars', 'Godefroy', 'Guibert', 'Guilhabert', 'Guillaume', 'Guillot', 'Henri', 'Hernaut', 'Hugues', 'Jacques', 'Jean', 'Julien', 'Loup', 'Maitre', 'Martin', 'Mathieu', 'Maurice', 'Michel', 'Olivier', 'Othon', 'Philippe', 'Pierre', 'Pons', 'Prades', 'Rainouart', 'Rannulf', 'Raymond', 'Renart', 'Renauld', 'Richard', 'Robert', 'Roger', 'Roscelin', 'Saire', 'Simon', 'Théodose', 'Tybald', 'Valentin', 'Vivien'
+'AbÃ©lard', 'AdhÃ©mar', 'Alain', 'Albert', 'Alizandre', 'AmÃ©lien', 'Amiel', 'AndrÃ©', 'Anselm', 'Archambaud', 'Arnaud', 'Aubert', 'Aubry', 'Baldwin', 'BarthÃ©lemy', 'Benet', 'BenoÃ®t', 'BÃ©renger', 'Bernard', 'Berthomieu', 'Bertrand', 'ChrÃ©tien', 'Constantin', 'Emmanuel', 'Erard', 'Eustace', 'Evre', 'FrÃ©dÃ©ric', 'Fulbert', 'Fulk', 'Gaillard', 'Galland', 'Gaston', 'Gaudri', 'Gautier', 'Geoffroy', 'Gerbert', 'Germain', 'Girard', 'Girars', 'Godefroy', 'Guibert', 'Guilhabert', 'Guillaume', 'Guillot', 'Henri', 'Hernaut', 'Hugues', 'Jacques', 'Jean', 'Julien', 'Loup', 'Maitre', 'Martin', 'Mathieu', 'Maurice', 'Michel', 'Olivier', 'Othon', 'Philippe', 'Pierre', 'Pons', 'Prades', 'Rainouart', 'Rannulf', 'Raymond', 'Renart', 'Renauld', 'Richard', 'Robert', 'Roger', 'Roscelin', 'Saire', 'Simon', 'ThÃ©odose', 'Tybald', 'Valentin', 'Vivien'
 ]
 gen_data["French_General_female_common"] = [
 'Agnes', 'Alice', 'Blanche', 'Eleanor', 'Isabeau', 'Isabelle', 'Jeanne', 'Marguerite', 'Matilda', 'Maude'
 ]
 gen_data["French_General_female_uncommon"] = [
-'Agnes', 'Alazaïs', 'Alesta', 'Algaia', 'Alianore', 'Alice', 'Alicia', 'Alissende', 'Arnaude', 'Aude', 'Ava', 'Bartholomette', 'Basina', 'Béatrice', 'Bérengère', 'Bernadette', 'Bertrade', 'Blanche', 'Brune', 'Brunissende', 'Charlotte', 'Christine', 'Clotilde', 'Constance', 'Diane', 'Eglantine', 'Emersende', 'Ennasuitte', 'Ermengarde', 'Ermentrude', 'Esclarmonde', 'Esperte', 'Estelle', 'Fabrisse', 'Florence', 'Gabrielle', 'Gaillarde', 'Gaude', 'Gauzia', 'Gentile', 'Gerberge', 'Grazide', 'Guillemette', 'Hawise', 'Héloïse', 'Honors', 'Huguette', 'Isabeau', 'Isabella', 'Isabelle', 'Jacotte', 'Jambicque', 'Jeanne', 'Longarine', 'Louve', 'Marguerite', 'Marie', 'Marquise', 'Mathena', 'Matilda', 'Maura', 'Mengarde', 'Oisille', 'Phillipa', 'Raymonde', 'Rixende', 'Rousse', 'Serena', 'Stéphanie', 'Sybilla', 'Sybille', 'Thomassia', 'Vuissance'
+'Agnes', 'AlazaÃ¯s', 'Alesta', 'Algaia', 'Alianore', 'Alice', 'Alicia', 'Alissende', 'Arnaude', 'Aude', 'Ava', 'Bartholomette', 'Basina', 'BÃ©atrice', 'BÃ©rengÃ¨re', 'Bernadette', 'Bertrade', 'Blanche', 'Brune', 'Brunissende', 'Charlotte', 'Christine', 'Clotilde', 'Constance', 'Diane', 'Eglantine', 'Emersende', 'Ennasuitte', 'Ermengarde', 'Ermentrude', 'Esclarmonde', 'Esperte', 'Estelle', 'Fabrisse', 'Florence', 'Gabrielle', 'Gaillarde', 'Gaude', 'Gauzia', 'Gentile', 'Gerberge', 'Grazide', 'Guillemette', 'Hawise', 'HÃ©loÃ¯se', 'Honors', 'Huguette', 'Isabeau', 'Isabella', 'Isabelle', 'Jacotte', 'Jambicque', 'Jeanne', 'Longarine', 'Louve', 'Marguerite', 'Marie', 'Marquise', 'Mathena', 'Matilda', 'Maura', 'Mengarde', 'Oisille', 'Phillipa', 'Raymonde', 'Rixende', 'Rousse', 'Serena', 'StÃ©phanie', 'Sybilla', 'Sybille', 'Thomassia', 'Vuissance'
 ]
 
 gen_data["French_General_male_name"] = {
@@ -25,13 +25,13 @@ gen_data["Norman_male_common"] = [
 'Baudouin', 'Gautier', 'Geoffroy', 'Guillaume', 'Guy', 'Henri', 'Hugues', 'Jean', 'Pierre', 'Robert'
 ]
 gen_data["Norman_male_uncommon"] = [
-'Adrian', 'Alexandre', 'Alfred', 'Almaric', 'Alured', 'Amaury', 'Ambrose', 'André', 'Antoine', 'Barthélémy', 'Baudouin', 'Berengar', 'Bernard', 'Bertrand', 'Blaise', 'Blondel', 'Bohemond', 'Charles', 'Claude', 'Constant', 'Denis', 'Dolfin', 'Drogo', 'Eustace', 'Falco', 'François', 'Fulco', 'Fulk', 'Gabriel', 'Gallien', 'Geoffrey', 'Georges', 'Gerbert', 'Gervase', 'Gilbert', 'Gilles', 'Giraud', 'Godfrey', 'Guy', 'Guillaume', 'Henri', 'Hubert', 'Hugh', 'Hugues', 'Humphrey', 'Isembart', 'Ivo', 'Jacques', 'Jean', 'Jerome', 'Jocelin', 'Julien', 'Lanfranc', 'Laurens', 'Leopold', 'Louis', 'Marc', 'Marin', 'Martin', 'Matthew', 'Milo', 'Nicholas', 'Nigel', 'Noel', 'Odo', 'Osbern', 'Philip', 'Raimund', 'Ralph', 'Raoul', 'René', 'Richard', 'Robert', 'Roger', 'Rolle', 'Romain', 'Simon', 'Stephen', 'Tancred', 'Thomas', 'Vincent', 'Wace', 'Waleran', 'Walter'
+'Adrian', 'Alexandre', 'Alfred', 'Almaric', 'Alured', 'Amaury', 'Ambrose', 'AndrÃ©', 'Antoine', 'BarthÃ©lÃ©my', 'Baudouin', 'Berengar', 'Bernard', 'Bertrand', 'Blaise', 'Blondel', 'Bohemond', 'Charles', 'Claude', 'Constant', 'Denis', 'Dolfin', 'Drogo', 'Eustace', 'Falco', 'FranÃ§ois', 'Fulco', 'Fulk', 'Gabriel', 'Gallien', 'Geoffrey', 'Georges', 'Gerbert', 'Gervase', 'Gilbert', 'Gilles', 'Giraud', 'Godfrey', 'Guy', 'Guillaume', 'Henri', 'Hubert', 'Hugh', 'Hugues', 'Humphrey', 'Isembart', 'Ivo', 'Jacques', 'Jean', 'Jerome', 'Jocelin', 'Julien', 'Lanfranc', 'Laurens', 'Leopold', 'Louis', 'Marc', 'Marin', 'Martin', 'Matthew', 'Milo', 'Nicholas', 'Nigel', 'Noel', 'Odo', 'Osbern', 'Philip', 'Raimund', 'Ralph', 'Raoul', 'RenÃ©', 'Richard', 'Robert', 'Roger', 'Rolle', 'Romain', 'Simon', 'Stephen', 'Tancred', 'Thomas', 'Vincent', 'Wace', 'Waleran', 'Walter'
 ]
 gen_data["Norman_female_common"] = [
 'Agnes', 'Alice', 'Blanche', 'Eleanor', 'Isabeau', 'Isabelle', 'Jeanne', 'Marguerite', 'Matilda', 'Maude'
 ]
 gen_data["Norman_female_uncommon"] = [
-'Adela', 'Adelaide', 'Adelasia', 'Agnes', 'Alice', 'Amice', 'Anne', 'Antoinette', 'Anyas', 'Arletta', 'Aveline', 'Avis', 'Barbe', 'Berengaria', 'Bertha', 'Catherine', 'Charlotte', 'Constance', 'Diane', 'Eleanor', 'Elenor', 'Emma', 'Emmeline', 'Fleurie', 'Florence', 'Florymonde', 'Françoise', 'Geneviève', 'Hélène', 'Isabeau', 'Isabel', 'Isabella', 'Jacquelle', 'Jeanne', 'Jocelyn', 'Joyce', 'Katherine', 'Laura', 'Laurente', 'Lauretta', 'Leonor', 'Louise', 'Lucque', 'Madeleine', 'Margery', 'Marguerite', 'Marie', 'Marthe', 'Matilda', 'Maude', 'Melisande', 'Melisine', 'Millicent', 'Muriel', 'Oriel', 'Philippa', 'Rachel', 'Rosamond', 'Rose', 'Sibyl', 'Sibylla', 'Suzanne', 'Victoire', 'Yolande', 'Yvonne'
+'Adela', 'Adelaide', 'Adelasia', 'Agnes', 'Alice', 'Amice', 'Anne', 'Antoinette', 'Anyas', 'Arletta', 'Aveline', 'Avis', 'Barbe', 'Berengaria', 'Bertha', 'Catherine', 'Charlotte', 'Constance', 'Diane', 'Eleanor', 'Elenor', 'Emma', 'Emmeline', 'Fleurie', 'Florence', 'Florymonde', 'FranÃ§oise', 'GeneviÃ¨ve', 'HÃ©lÃ¨ne', 'Isabeau', 'Isabel', 'Isabella', 'Jacquelle', 'Jeanne', 'Jocelyn', 'Joyce', 'Katherine', 'Laura', 'Laurente', 'Lauretta', 'Leonor', 'Louise', 'Lucque', 'Madeleine', 'Margery', 'Marguerite', 'Marie', 'Marthe', 'Matilda', 'Maude', 'Melisande', 'Melisine', 'Millicent', 'Muriel', 'Oriel', 'Philippa', 'Rachel', 'Rosamond', 'Rose', 'Sibyl', 'Sibylla', 'Suzanne', 'Victoire', 'Yolande', 'Yvonne'
 ]
 
 gen_data["Norman_male_name"] = {
@@ -44,36 +44,35 @@ gen_data["Norman_female_name"] = {
 }
 
 
-gen_data["Provençal_male_common"] = [
+gen_data["ProvenÃ§al_male_common"] = [
 'Baudouin', 'Gautier', 'Geoffroy', 'Guillaume', 'Guy', 'Henri', 'Hugues', 'Jean', 'Pierre', 'Robert'
 ]
-gen_data["Provençal_male_uncommon"] = [
-'Abit', 'Aimery', 'Albèrt', 'Amelh', 'Amelian', 'Andrieu', 'Arnault', 'Arnaut', 'Asemar', 'Barral', 'Beneset', 'Berenguièr', 'Bernart', 'Bertomieu', 'Bertran', 'Boé', 'Bonnet', 'Cercamon', 'Claud', 'Clément', 'Danyel', 'David', 'Emeric', 'Estold', 'Étienne', 'Fabre', 'Felip', 'Folquet', 'François', 'Gaspard', 'Gaston', 'Gaucelm', 'German', 'Girault', 'Gualhard', 'Guido', 'Guilhabèrt', 'Guilhèm', 'Guilhòt', 'Huc', 'Irénée', 'Isarn', 'Jasmin', 'Jaufré', 'Jaume', 'Jean', 'Jogues', 'Josephe', 'Julian', 'Kyot', 'Lop', 'Louis', 'Marcabrun', 'Martin', 'Maurice', 'Mèstre', 'Miquèl', 'Olivier', 'Oth', 'Oton', 'Paul', 'Pèire', 'Peyrot', 'Pierol', 'Pons', 'Pradas', 'Raimon', 'Raimond', 'Savario', 'Seguin', 'Simon', 'Toussaint', 'Tristan', 'Valentin', 'Vidal', 'Walter'
+gen_data["ProvenÃ§al_male_uncommon"] = [
+'Abit', 'Aimery', 'AlbÃ¨rt', 'Amelh', 'Amelian', 'Andrieu', 'Arnault', 'Arnaut', 'Asemar', 'Barral', 'Beneset', 'BerenguiÃ¨r', 'Bernart', 'Bertomieu', 'Bertran', 'BoÃ©', 'Bonnet', 'Cercamon', 'Claud', 'ClÃ©ment', 'Danyel', 'David', 'Emeric', 'Estold', 'Ã‰tienne', 'Fabre', 'Felip', 'Folquet', 'FranÃ§ois', 'Gaspard', 'Gaston', 'Gaucelm', 'German', 'Girault', 'Gualhard', 'Guido', 'GuilhabÃ¨rt', 'GuilhÃ¨m', 'GuilhÃ²t', 'Huc', 'IrÃ©nÃ©e', 'Isarn', 'Jasmin', 'JaufrÃ©', 'Jaume', 'Jean', 'Jogues', 'Josephe', 'Julian', 'Kyot', 'Lop', 'Louis', 'Marcabrun', 'Martin', 'Maurice', 'MÃ¨stre', 'MiquÃ¨l', 'Olivier', 'Oth', 'Oton', 'Paul', 'PÃ¨ire', 'Peyrot', 'Pierol', 'Pons', 'Pradas', 'Raimon', 'Raimond', 'Savario', 'Seguin', 'Simon', 'Toussaint', 'Tristan', 'Valentin', 'Vidal', 'Walter'
 ]
-gen_data["Provençal_female_common"] = [
+gen_data["ProvenÃ§al_female_common"] = [
 'Agnes', 'Alice', 'Blanche', 'Eleanor', 'Isabeau', 'Isabelle', 'Jeanne', 'Marguerite', 'Matilda', 'Maude'
 ]
-gen_data["Provençal_female_uncommon"] = [
-'Alasais', 'Alesta', 'Algaia', 'Alicia', 'Aliénor', 'Alissenda', 'Anhés', 'Arnauda', 'Auda', 'Beatriz', 'Berenguièira', 'Bernadeta', 'Bertomièva', 'Blanca', 'Blandina', 'Bruna', 'Brunissenda', 'Cassanea', 'Cavilia', 'Constancia', 'Dolça', 'Eglantina', 'Emersenda', 'Ermengarda', 'Ermessinda', 'Esclarmonda', 'Esperta', 'Estela', 'Estevena', 'Eugénie', 'Eva', 'Fabricia', 'Faydide', 'Felipa', 'Flamença', 'Floréncia', 'Florine', 'Galharda', 'Gauda', 'Gauzia', 'Gentila', 'Grazida', 'Griselde', 'Guilhemeta', 'Guirande', 'Imperia', 'Jacotina', 'Joana', 'Loba', 'Mabilia', 'Mamille', 'Margherita', 'Maria', 'Marquesina', 'Matena', 'Maura', 'Mengarda', 'Misse', 'Nova', 'Onors', 'Raimonda', 'Rixenda', 'Rossa', 'Serena', 'Sibilia', 'Sybelle', 'Tomassia', 'Torqueri', 'Ugueta', 'Vuissancia', 'Yolande'
+gen_data["ProvenÃ§al_female_uncommon"] = [
+'Alasais', 'Alesta', 'Algaia', 'Alicia', 'AliÃ©nor', 'Alissenda', 'AnhÃ©s', 'Arnauda', 'Auda', 'Beatriz', 'BerenguiÃ¨ira', 'Bernadeta', 'BertomiÃ¨va', 'Blanca', 'Blandina', 'Bruna', 'Brunissenda', 'Cassanea', 'Cavilia', 'Constancia', 'DolÃ§a', 'Eglantina', 'Emersenda', 'Ermengarda', 'Ermessinda', 'Esclarmonda', 'Esperta', 'Estela', 'Estevena', 'EugÃ©nie', 'Eva', 'Fabricia', 'Faydide', 'Felipa', 'FlamenÃ§a', 'FlorÃ©ncia', 'Florine', 'Galharda', 'Gauda', 'Gauzia', 'Gentila', 'Grazida', 'Griselde', 'Guilhemeta', 'Guirande', 'Imperia', 'Jacotina', 'Joana', 'Loba', 'Mabilia', 'Mamille', 'Margherita', 'Maria', 'Marquesina', 'Matena', 'Maura', 'Mengarda', 'Misse', 'Nova', 'Onors', 'Raimonda', 'Rixenda', 'Rossa', 'Serena', 'Sibilia', 'Sybelle', 'Tomassia', 'Torqueri', 'Ugueta', 'Vuissancia', 'Yolande'
 ]
 
-gen_data["Provençal_male_name"] = {
-    '1-80': '{Provençal_male_common}',
-    '81-100': '{Provençal_male_uncommon}'
+gen_data["ProvenÃ§al_male_name"] = {
+    '1-80': '{ProvenÃ§al_male_common}',
+    '81-100': '{ProvenÃ§al_male_uncommon}'
 }
-gen_data["Provençal_female_name"] = {
-    '1-80': '{Provençal_female_common}',
-    '81-100': '{Provençal_female_uncommon}'
+gen_data["ProvenÃ§al_female_name"] = {
+    '1-80': '{ProvenÃ§al_female_common}',
+    '81-100': '{ProvenÃ§al_female_uncommon}'
 }
 
 gen_data["French_male_name"] = {
-    '1-33': '{Provençal_male_name}',
+    '1-33': '{ProvenÃ§al_male_name}',
     '34-66': '{Norman_male_name}',
     '67-100': '{French_General_male_name}'
 }
 gen_data["French_female_name"] = {
-    '1-33': '{Provençal_female_name}',
+    '1-33': '{ProvenÃ§al_female_name}',
     '34-66': '{Norman_female_name}',
     '67-100': '{French_General_female_name}'
 }
-
