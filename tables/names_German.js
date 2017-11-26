b3a37697220b41cd86215578578ead33511ca22a
Ted Tschopp <ted@tschopp.org>
2017-11-26 14:00:04 -0800

    UTF-8

--- tables/names_German.js
+++ tables/names_German.js
@@ -2,13 +2,13 @@ gen_data["German_male_common"] = [
 'Friedrich', 'Georg', 'Heinrich', 'Hermann', 'Johannes', 'Konrad', 'Michael', 'Nikolaus', 'Peter', 'Ulrich'
 ]
 gen_data["German_male_uncommon"] = [
-'Adam', 'Albert', 'Albrecht', 'Andreas', 'Arnold', 'Balthasar', 'Bartusch', 'Berchtold', 'Bernhard', 'Bertold', 'Bertolf', 'Bruno', 'Burgolt', 'Caspar', 'Christoff', 'Clement', 'Conrad', 'David', 'Dietrich', 'Eberhard', 'Francze', 'Frederich', 'Friderich', 'Friedrich', 'Fritsche', 'Gabriel', 'Gebehart', 'Georg', 'Gerhard', 'Gisko', 'Gottfrid', 'Gregor', 'Günczel', 'Gunther', 'Hannes', 'Hans', 'Hansel', 'Heinczel', 'Heinke', 'Heinrich', 'Helwig', 'Hempel', 'Hentschel', 'Herbord', 'Hermann', 'Hertwig', 'Jacob', 'Jekel', 'Johannes', 'Jorge', 'Jost', 'Jurgen', 'Konrad', 'Lamprecht', 'Lorencz', 'Ludwig', 'Luther', 'Lüthold', 'Lütke', 'Manfred', 'Marcus', 'Martin', 'Mathis', 'Michael', 'Nikolaus', 'Örtel', 'Ortolf', 'Oswald', 'Otto', 'Paul', 'Peczold', 'Peter', 'Reinfrid', 'Reinhard', 'Rüdel', 'Rüdger', 'Rudolf', 'Ruprecht', 'Sander', 'Siegfrid', 'Sigmund', 'Steffan', 'Symon', 'Thomas', 'Till', 'Tristram', 'Ulrich', 'Volkmar', 'Walther', 'Wenczel', 'Wernher', 'Winand', 'Wiprecht', 'Wolf', 'Wolfgang', 'Wolfhart', 'Wolfram'
+'Adam', 'Albert', 'Albrecht', 'Andreas', 'Arnold', 'Balthasar', 'Bartusch', 'Berchtold', 'Bernhard', 'Bertold', 'Bertolf', 'Bruno', 'Burgolt', 'Caspar', 'Christoff', 'Clement', 'Conrad', 'David', 'Dietrich', 'Eberhard', 'Francze', 'Frederich', 'Friderich', 'Friedrich', 'Fritsche', 'Gabriel', 'Gebehart', 'Georg', 'Gerhard', 'Gisko', 'Gottfrid', 'Gregor', 'GÃ¼nczel', 'Gunther', 'Hannes', 'Hans', 'Hansel', 'Heinczel', 'Heinke', 'Heinrich', 'Helwig', 'Hempel', 'Hentschel', 'Herbord', 'Hermann', 'Hertwig', 'Jacob', 'Jekel', 'Johannes', 'Jorge', 'Jost', 'Jurgen', 'Konrad', 'Lamprecht', 'Lorencz', 'Ludwig', 'Luther', 'LÃ¼thold', 'LÃ¼tke', 'Manfred', 'Marcus', 'Martin', 'Mathis', 'Michael', 'Nikolaus', 'Ã–rtel', 'Ortolf', 'Oswald', 'Otto', 'Paul', 'Peczold', 'Peter', 'Reinfrid', 'Reinhard', 'RÃ¼del', 'RÃ¼dger', 'Rudolf', 'Ruprecht', 'Sander', 'Siegfrid', 'Sigmund', 'Steffan', 'Symon', 'Thomas', 'Till', 'Tristram', 'Ulrich', 'Volkmar', 'Walther', 'Wenczel', 'Wernher', 'Winand', 'Wiprecht', 'Wolf', 'Wolfgang', 'Wolfhart', 'Wolfram'
 ]
 gen_data["German_female_common"] = [
 'Adelheid', 'Agnes', 'Anna', 'Barbara', 'Christine', 'Elisabeth', 'Gerhaus', 'Katharina', 'Kunigunde', 'Margarete'
 ]
 gen_data["German_female_uncommon"] = [
-'Adelheid', 'Agathe', 'Agnes', 'Alke', 'Anna', 'Apollonia', 'Aythe', 'Barbara', 'Beate', 'Benedicta', 'Bertha', 'Bertrude', 'Cecilia', 'Christine', 'Clare', 'Czylle', 'Demut', 'Dorothye', 'Elisabeth', 'Elze', 'Enede', 'Engeltrude', 'Enneleyn', 'Ermegard', 'Eufemia', 'Femeke', 'Gerhaus', 'Gerke', 'Gertrude', 'Gerusch', 'Grite', 'Hedwig', 'Helena', 'Herburg', 'Herlinde', 'Hezke', 'Hilde', 'Hildeberg', 'Hildegund', 'Hilusch', 'Ilse', 'Irmel', 'Irmeltrud', 'Jutte', 'Katharina', 'Katusch', 'Kethe', 'Kirstyn', 'Kunigunde', 'Künne', 'Lena', 'Libeste', 'Libusch', 'Liphilt', 'Lucie', 'Lusche', 'Magdalena', 'Maneth', 'Margarete', 'Marie', 'Marusch', 'Mechtilde', 'Mecze', 'Merlyn', 'Müte', 'Neleke', 'Osanna', 'Osterhild', 'Otilge', 'Ottilie', 'Phye', 'Sabine', 'Salmey', 'Sanne', 'Sophie', 'Ursula', 'Ute', 'Veronica', 'Ysentrude'
+'Adelheid', 'Agathe', 'Agnes', 'Alke', 'Anna', 'Apollonia', 'Aythe', 'Barbara', 'Beate', 'Benedicta', 'Bertha', 'Bertrude', 'Cecilia', 'Christine', 'Clare', 'Czylle', 'Demut', 'Dorothye', 'Elisabeth', 'Elze', 'Enede', 'Engeltrude', 'Enneleyn', 'Ermegard', 'Eufemia', 'Femeke', 'Gerhaus', 'Gerke', 'Gertrude', 'Gerusch', 'Grite', 'Hedwig', 'Helena', 'Herburg', 'Herlinde', 'Hezke', 'Hilde', 'Hildeberg', 'Hildegund', 'Hilusch', 'Ilse', 'Irmel', 'Irmeltrud', 'Jutte', 'Katharina', 'Katusch', 'Kethe', 'Kirstyn', 'Kunigunde', 'KÃ¼nne', 'Lena', 'Libeste', 'Libusch', 'Liphilt', 'Lucie', 'Lusche', 'Magdalena', 'Maneth', 'Margarete', 'Marie', 'Marusch', 'Mechtilde', 'Mecze', 'Merlyn', 'MÃ¼te', 'Neleke', 'Osanna', 'Osterhild', 'Otilge', 'Ottilie', 'Phye', 'Sabine', 'Salmey', 'Sanne', 'Sophie', 'Ursula', 'Ute', 'Veronica', 'Ysentrude'
 ]
 
 gen_data["German_male_name"] = {
@@ -19,9 +19,3 @@ gen_data["German_female_name"] = {
     '1-80': '{German_female_common}',
     '81-100': '{German_female_uncommon}'
 }
-
-
-
-
-
-
