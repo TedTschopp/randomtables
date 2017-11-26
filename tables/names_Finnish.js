b2b7613dd907b0a7b609096be2d82ce693596898
Ted Tschopp <ted@tschopp.org>
2017-11-26 13:58:46 -0800

    UTF-8

--- tables/names_Finnish.js
+++ tables/names_Finnish.js
@@ -1,6 +1,7 @@
 gen_data["Finnish_male_name"] = [
-'Aarnikka', 'Asikko', 'Aslak', 'Berend', 'Bertil', 'Bjˆrn', 'Erik', 'Ervasti', 'Haakon', 'H‰sikk‰', 'Heikki', 'Henrik', 'Johannes', 'Kauppi', 'Luukas', 'Menikko', 'Pentti', 'Reko', 'Tuokki', 'V‰mm‰'
+'Aarnikka', 'Asikko', 'Aslak', 'Berend', 'Bertil', 'Bj√∂rn', 'Erik', 'Ervasti', 'Haakon', 'H√§sikk√§', 'Heikki', 'Henrik', 'Johannes', 'Kauppi', 'Luukas', 'Menikko', 'Pentti', 'Reko', 'Tuokki', 'V√§mm√§'
 ]
+
 gen_data["Finnish_male_name"] = [
-'Adelitsa', 'Agnes', 'Dorathea', 'Elin', 'Elseby', 'Esteri', 'Gertrud', 'Greta', 'Gudlˆg', 'Ingrid', 'Johanna', 'Katariina', 'L‰ps‰', 'Lucia', 'Margit', 'M‰rta', 'Miksit‰r', 'Rikitsa', 'Sigrid', 'Suomi'
+'Adelitsa', 'Agnes', 'Dorathea', 'Elin', 'Elseby', 'Esteri', 'Gertrud', 'Greta', 'Gudl√∂g', 'Ingrid', 'Johanna', 'Katariina', 'L√§ps√§', 'Lucia', 'Margit', 'M√§rta', 'Miksit√§r', 'Rikitsa', 'Sigrid', 'Suomi'
 ]
