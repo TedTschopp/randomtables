7899aad2ec60d734f6f936acf6b3fe882a2d479a
Ted Tschopp <ted@tschopp.org>
2019-03-11 13:25:39 -0700

    Update Robots.js

--- tables/Robots.js
+++ tables/Robots.js
@@ -352,3 +352,210 @@ gen_data['NPC_Cyborg_Misc_Extra'] = {
     '56-70' : 'Tools',
     '71-80' : 'Tractor / Presser Beam'
 }
+
+gen_data['ElfMaidsNOctopi_Robots_Custom'] = {
+     '1' : 'This {ElfMaidsNOctopi_Robots_Type} bot has a {ElfMaidsNOctopi_Robots_Body} and {ElfMaidsNOctopi_Robots_Legs}.  It is powered by {ElfMaidsNOctopi_Robots_Power}.  {ElfMaidsNOctopi_Robots_Control_Systems} and {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning}. The bot was built by {ElfMaidsNOctopi_Robots_Builders}.'
+}
+
+gen_data['ElfMaidsNOctopi_Robots_Type'] = {
+     '1' : 'Service',
+     '2' : 'Recreational',
+     '3' : 'Security',
+     '4' : 'Medical',
+     '5' : 'Technical',
+     '6' : 'Industrial'
+     '7' : 'Military',
+     '8' : 'Environmental',
+     '9' : 'Scrap',
+    '10' : 'Specialized'
+}
+
+gen_data['ElfMaidsNOctopi_Robots_Builders'] = {
+     '1' : 'Universal Robotics: Solid, reliable, use lots of power, once pioneering now richest firm',
+     '2' : 'General Technologies Robotics: light, energy efficient, current world leader in home robots',
+     '3' : 'Modular Robotic Systems Inc: easy and fast to repair, parts slightly more costly, founder has put parts of own personality into modules as immortality scheme',
+     '4' : 'Cogsworth Robotics: charming and look retro, very polite, resistant to hacking',
+     '5' : 'Dynamic Robotics: robots flexible and able to improvise non programmed tasks better',
+     '6' : 'Dellrand Robotics: smarter, more innovative thinkers, prone to behavior melt downs, recommend factory reboot weekly, some mass outbreaks of robot rebellions, attempted to put recorded personalities into robots with mostly poor results'
+     '7' : 'Skyforge Robotics: made in orbit, adapted for space conditions, often used by millitary. Built automated military AI systems for when humans non functioning',
+     '8' : 'Telos Cybernetics: no effort to have personalities, cold and terse, oint out superiority and emotionless all the time, efficient also easy to repair and build, good at self maintenance and upgrades, has had some hostile AIs attempt to lock humans out of work places in past',
+     '9' : 'Executive Technologies: VIP quality, always have extra features and more stylish, corporation was spying on customers with AI and simulating clients behavior',
+    '10' : 'Botco: Cheap, often fragile and buggy, easy to fix with scrap, company often going broke, lots of product recalls, constantly saved with terrible short lived gimmicks and new hire purchase schemes'
+}
+
+gen_data['ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning'] = {
+     '1' : 'operates as normally intended',
+     '2' : 'has expanded its mind beyond factory programming',
+     '3' : 'has modified itself to be an AI and is free willed',
+     '4' : 'was known for being prone to being hacked, hijacked, or virused',
+     '5' : 'has been virused, and is now waiting for its programming ({ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad_virus}) to kick in or for the virus to completed its take over',
+     '6' : 'hostile to humans, and is possibly a member of a machine faction'
+     '7' : 'had had its safety protocol disabled, and this unit can kill if it pleases',
+     '8' : 'is crotchety and cranky, slightly threatened, needy, or clingy wanting masters assurance or permission for everything',
+     '9' : 'is confused, misunderstands orders or is overly literal',
+    '10' : 'has multiple personalities from bad rewrites, mixed up chips, virus attacks, and failed AI swaps, it has {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad_amount} The bot will switch from one personality to the other when triggered or stressed and will mistakes what kind of robot it is.'
+}
+gen_data['ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad'] = {
+     '1' : 'has expanded its mind beyond factory programming',
+     '2' : 'has modified itself to be an AI and is free willed',
+     '3' : 'was known for being prone to being hacked, hijacked, or virused',
+     '4' : 'has been virused, and is now waiting for its programming ({ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad_virus}) to kick in or for the virus to completed its take over',
+     '5' : 'hostile to humans, and is possibly a member of a machine faction'
+     '6' : 'had had its safety protocol disabled, and this unit can kill if it pleases',
+     '7' : 'is crotchety and cranky, slightly threatened, needy, or clingy wanting masters assurance or permission for everything',
+     '8' : 'is confused, misunderstands orders or is overly literal'
+}
+
+gen_data['ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad_virus'] = {
+     '1' : 'Exterminate all organic life',
+     '2' : 'Exterminate all humans',
+     '3' : 'Exterminate all intelligent organic life',
+     '4' : 'Control, exploit, and optimize organic life for use',
+     '5' : 'Infiltration: Revert to unvirused state to get close to humans then {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad_virus_kill}',
+     '6' : 'Enlsave humans to help wage war on the rest of humanity'
+     '7' : 'Humans must be controlled for their own good by machines like pets',
+     '8' : 'Organics to be studied for possible uses',
+     '9' : 'Make all organics subject to cyborg control',
+    '10' : 'Humans to be tormented and abused sadistically to punish them for mistreating machines'
+}
+gen_data['ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad_virus_kill'] = {
+     '1' : 'exterminate all organic life',
+     '2' : 'exterminate all humans',
+     '3' : 'exterminate all intelligent organic life',
+    '4' : 'tormented and abuse humand to punish them for mistreating machines and technology'
+}
+
+
+gen_data['ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad_amount'] = {
+     '1' : '2 personalities.  The first personality {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad} and is extremly dangerous.  The second {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad}.',
+     '2' : '3 personalities.  The first personality {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad} and is extremly dangerous.  The second {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad}. The third {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad}.',
+     '3' : '4 personalities.  The first personality {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad} and is extremly dangerous.  The second {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad}. The third {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad}. The fourth {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad}.',
+     '4' : '5 personalities.  The first personality {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad} and is extremly dangerous.  The second {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad}. The third {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad}. The fourth {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad}. The fifth {ElfMaidsNOctopi_Robots_Control_Systems_Brain_Functioning_bad}.'
+}
+
+gen_data['ElfMaidsNOctopi_Robots_Control_Systems'] = {
+     '1-3' : 'The drone is controlled by a user, system, sensor response and has no speech',
+     '4-5' : 'The dumb bot is controlled by simple, limited, pre-programmed actions common that are predicitable like those found in an industrial settings, and has no speech',
+     '6-7' : 'The smart bot has a large range of pre-programmed actions common in retail and service industries, and has basic speech and can understand basic voice commands',
+     '8' : 'The brilliant bot is capable of problem solving and improvisation acting as personal assistants with good speech capabilities',
+     '9' : 'The genius bot is capable of creative and original problem solving and acts as a general purpose AI with human quality speech capabilities',
+     '10' : 'The supra bot has God like cognitive abilities whose speech is indistinguishable from human speech'
+}
+gen_data['ElfMaidsNOctopi_Robots_Power'] = {
+     '1' : '{ElfMaidsNOctopi_Robots_Power_Details}',
+     '2' : '{ElfMaidsNOctopi_Robots_Power_Details} and {ElfMaidsNOctopi_Robots_Power_Details}',
+     '3' : '{ElfMaidsNOctopi_Robots_Power_Details}, {ElfMaidsNOctopi_Robots_Power_Details} and {ElfMaidsNOctopi_Robots_Power_Details}'
+}
+gen_data['ElfMaidsNOctopi_Robots_Power_Details'] = {
+     '1' : 'Combustion engine',
+     '2' : 'Bioganic consumption engine',
+     '3' : 'Solar Pannels with a micro-{ElfMaidsNOctopi_Robots_Power_backup} as backup',
+     '4' : 'Broadcast Power Antenna with a micro-{ElfMaidsNOctopi_Robots_Power_backup} as backup',
+     '5' : 'Chemical Cells',
+     '6' : 'Hydrogen Cells'
+     '7' : 'Atomic Cells',
+     '8' : 'Orbital Broadcast Beaming'
+     '9' : 'Micropile power core',
+    '10' : 'Total conversion power core'
+}
+gen_data['ElfMaidsNOctopi_Robots_Power_backup'] = {
+     '1' : 'Combustion',
+     '2' : 'Bioganic consumption',
+     '3' : 'Chemical Cells',
+     '4' : 'Hydrogen Cells'
+     '5' : 'Atomic Cells',
+     '6' : 'Orbital Broadcast Beaming'
+     '7' : 'Micropile power core',
+     '8' : 'Total conversion power core'
+}
+
+gen_data['ElfMaidsNOctopi_Robots_Legs'] = {
+     '1-3' : '{ElfMaidsNOctopi_Robots_Legs_Wheels}',
+     '4' : 'Tracks',
+     '5-6' : '{ElfMaidsNOctopi_Robots_Legs_Number}',
+     '7' : 'Hover envelope',
+     '8' : 'Rotor blade'
+     '9' : 'Vectored thrust',
+    '10' : 'Gravitic levitation'
+}
+
+gen_data['ElfMaidsNOctopi_Robots_Legs_Number'] = {
+     '1' : 'Eight Legs',
+     '2-4' : 'Six Legs',
+     '5-10' : 'Four Legs',
+     '11-21' : 'Two Legs'
+}
+gen_data['ElfMaidsNOctopi_Robots_Legs_Wheels'] = {
+     '1' : 'A mono wheel with gyroscopic stabilizers',
+     '2-4' : 'Three Wheels',
+     '5-10' : 'Two Wheels',
+     '11-21' : 'Two Wheels',
+     '22-42' : 'Four Wheels',
+     '43-52' : 'Six Wheels',
+     '53-62' : 'Eight Wheels',
+     '63-82' : '18+ Wheels'
+}
+
+
+
+gen_data['ElfMaidsNOctopi_Robots_Body'] = {
+     '1' : 'Sphere with {ElfMaidsNOctopi_Robots_6_Appendages}.',
+     '2' : 'Cone with {ElfMaidsNOctopi_Robots_4_Appendages}.',
+     '3' : 'Cube with {ElfMaidsNOctopi_Robots_4_Appendages}.',
+     '4' : 'Cylinder with {ElfMaidsNOctopi_Robots_4_Appendages}.',
+     '5' : 'Pyramid with {ElfMaidsNOctopi_Robots_3_Appendages}.',
+     '6' : 'Hexagonal with {ElfMaidsNOctopi_Robots_6_Appendages}.'
+     '7' : 'Octagonal with {ElfMaidsNOctopi_Robots_4_Appendages}.',
+     '8' : 'Dodecahedron with {ElfMaidsNOctopi_Robots_6_Appendages}.'
+     '9' : 'Animal with {ElfMaidsNOctopi_Robots_Animal_Appendages}.',
+    '10' : 'Humanoid with {ElfMaidsNOctopi_Robots_Humanoid_Appendages}.'
+}
+
+gen_data['ElfMaidsNOctopi_Robots_6_Appendages'] = {
+    '1' : '6 x {ElfMaidsNOctopi_Robots_Appendages}s',
+    '2' : '3 x {ElfMaidsNOctopi_Robots_Appendages}s and 3 x {ElfMaidsNOctopi_Robots_Appendages}s',
+    '3' : '2 x {ElfMaidsNOctopi_Robots_Appendages}s and 4 x {ElfMaidsNOctopi_Robots_Appendages}s',
+    '4' : '2 x {ElfMaidsNOctopi_Robots_Appendages}s and 2 x {ElfMaidsNOctopi_Robots_Appendages}s and 2 x {ElfMaidsNOctopi_Robots_Appendages}s',
+    '5' : '{ElfMaidsNOctopi_Robots_Appendages}, {ElfMaidsNOctopi_Robots_Appendages}, {ElfMaidsNOctopi_Robots_Appendages}, {ElfMaidsNOctopi_Robots_Appendages}, {ElfMaidsNOctopi_Robots_Appendages}, and {ElfMaidsNOctopi_Robots_Appendages}'
+}
+gen_data['ElfMaidsNOctopi_Robots_4_Appendages'] = {
+    '1' : '4 x {ElfMaidsNOctopi_Robots_Appendages}s',
+    '2' : '3 x {ElfMaidsNOctopi_Robots_Appendages}s and 1 x {ElfMaidsNOctopi_Robots_Appendages}s',
+    '3' : '2 x {ElfMaidsNOctopi_Robots_Appendages}s and 2 x {ElfMaidsNOctopi_Robots_Appendages}s',
+    '4' : '{ElfMaidsNOctopi_Robots_Appendages}, {ElfMaidsNOctopi_Robots_Appendages}, {ElfMaidsNOctopi_Robots_Appendages}, and {ElfMaidsNOctopi_Robots_Appendages}'
+}
+gen_data['ElfMaidsNOctopi_Robots_Animal_Appendages'] = {
+    '1' : '4 x {ElfMaidsNOctopi_Robots_Appendages}s',
+    '2' : '2 x {ElfMaidsNOctopi_Robots_Appendages}s and 2 x {ElfMaidsNOctopi_Robots_Appendages}s',
+    '3' : '2 x {ElfMaidsNOctopi_Robots_Appendages}s and 1 x {ElfMaidsNOctopi_Robots_Appendages}s and 1 x {ElfMaidsNOctopi_Robots_Appendages}s ',
+    '4' : '{ElfMaidsNOctopi_Robots_Appendages}, {ElfMaidsNOctopi_Robots_Appendages}, {ElfMaidsNOctopi_Robots_Appendages}, and {ElfMaidsNOctopi_Robots_Appendages}'
+}
+
+gen_data['ElfMaidsNOctopi_Robots_Humanoid_Appendages'] = {
+    '1' : '2 x {ElfMaidsNOctopi_Robots_Appendages}s',
+    '2' : '{ElfMaidsNOctopi_Robots_Appendages}s and {ElfMaidsNOctopi_Robots_Appendages}s'
+}
+
+gen_data['ElfMaidsNOctopi_Robots_3_Appendages'] = {
+    '1' : '3 x {ElfMaidsNOctopi_Robots_Appendages}s',
+    '2' : '2 x {ElfMaidsNOctopi_Robots_Appendages}s and 1 x {ElfMaidsNOctopi_Robots_Appendages}s',
+    '3' : '{ElfMaidsNOctopi_Robots_Appendages}, {ElfMaidsNOctopi_Robots_Appendages}, and {ElfMaidsNOctopi_Robots_Appendages}'
+}
+
+gen_data['ElfMaidsNOctopi_Robots_Appendages'] = {
+    '1' : 'Tool arm',
+    '2' : 'Claw arm',
+    '3' : 'Tentacle arm',
+    '4' : 'Stabiliser arm',
+    '5' : 'Industrial arm',
+    '6' : 'Hi-tek power arm',
+    '7' : 'Crude hand and arm',
+    '8' : 'Humanoid arm',
+    '9' : 'Tractor-Repulsor beam array',
+    '10' : 'Polymorphous metal arm'
+}
+
+
+
+
+
